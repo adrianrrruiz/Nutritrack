aa7c3a439941d7af65121e2621bd690a46828b33
clarck2547 <juandavidh76@gmail.com>
2024-10-19 19:20:44 -0500

    header, footer y landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
-import { RegistrosComponent } from './pages/registros/registros.component';
-import { AgregarComponent } from './pages/agregar/agregar.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { IdeasActivasComponent } from './pages/ideas-activas/ideas-activas.component';
-import { RetosActivosComponent } from './pages/retos-activos/retos-activos.component';
-import { IdeaNuevaComponent } from './pages/idea-nueva/idea-nueva.component';
-import { RetoNuevoComponent } from './pages/reto-nuevo/reto-nuevo.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscadorComponent } from './pages/buscador/buscador.component';
 import { IdeaDetailsComponent } from './pages/idea-details/idea-details.component';
+import { IdeaNuevaComponent } from './pages/idea-nueva/idea-nueva.component';
 import { IdeaRevisionComponent } from './pages/idea-revision/idea-revision.component';
-import {BuscadorComponent } from './pages/buscador/buscador.component';
+import { IdeasActivasComponent } from './pages/ideas-activas/ideas-activas.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { RegistrosComponent } from './pages/registros/registros.component';
+import { RetoNuevoComponent } from './pages/reto-nuevo/reto-nuevo.component';
+import { RetosActivosComponent } from './pages/retos-activos/retos-activos.component';
 
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
@@ -40,9 +41,10 @@ import {BuscadorComponent } from './pages/buscador/buscador.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
